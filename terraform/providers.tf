f2f45df683c51ea2e0492a75e523ba22d3f5850f
vinayboggarapu1983 <109145771+vinayboggarapu1983@users.noreply.github.com>
2022-11-05 06:35:22 +0000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -17,7 +17,8 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = "4.42.1"
-      credentials = file("accounts.json")
+      GOOGLE_APPLICATION_CREDENTIALS = file("sc_jenkins_terraform")
+      GOOGLE_CLOUD_KEYFILE_JSON = credentials('sc_jenkins_terraform')
     }
   }
 }
