7b073ff129146f521574b8a0b5c96e63bb83bdba
vinayboggarapu1983 <109145771+vinayboggarapu1983@users.noreply.github.com>
2022-11-06 07:14:54 +0000

    Update main.tf

--- terraform/gke-cluster-creation/main.tf
+++ terraform/gke-cluster-creation/main.tf
@@ -1,6 +1,20 @@
-provider "google" {
-  version = "~> 3.42.0"
-}
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Definition of local variables
+
+# Create GKE cluster
 module "gke_auth" {
   source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   depends_on   = [module.gke]
